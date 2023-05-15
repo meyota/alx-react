4af4cc573f8c6c4af22060aebeefb61b74fa84f9
meyota <meyota08@gmail.com>
2023-05-15 17:53:30 +0000

    web pack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,7 +3,7 @@ import _ from 'loadash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Click here to get started</p>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
