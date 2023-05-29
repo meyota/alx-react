f793bd2662a9687fef62834eb43c79cb5d9a9bb5
meyota <meyota08@gmail.com>
2023-05-29 04:01:29 -0700

    react

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,24 +1,14 @@
-import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
+test("returns current year", () => {
+  expect(getFullYear()).toBe(2022);
+});
 
-test("getFullYear returns the correct year", () => {
-    const year = getFullYear();
-    expect(year).toBe(2022)
-})
+test("correct footer copy", () => {
+  expect(getFooterCopy(true)).toBe("Holberton School");
+  expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+});
 
-test("returns `Holberton School` when `isIndex` is true", () => {
-    const footerText = getFooterCopy(true);
-    expect(footerText).toBe('Holberton School');
-})
-test("returns `Holberton School main dashboard` when `isIndex` is false", () => {
-    const footerText = getFooterCopy(false);
-    expect(footerText).toBe('Holberton School main dashboard');
-})
-test("returns string", () => {
-    const notificationText = getLatestNotification()
-    expect(typeof notificationText).toBe("string");
-})
-test("returns a specific string", () => {
-    const notificationText = getLatestNotification()
-    expect(notificationText).toBe("<strong>Urgent requirement</strong> - complete by EOD");
-})
+test("returns right notification", () => {
+  expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+});
