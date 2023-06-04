09dd75549df54330fdfc4543a1c5581809baedcc
meyota <meyota08@gmail.com>
2023-06-04 03:26:30 -0700

    react

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
-import logo from './holberton-logo.jpg';
+import logo from '../assets/holberton_logo.jpg';
 import './App.css';
-import { getFullYear, getFooterCopy } from './utils';
+import { getFooterCopy, getFullYear } from '../utils/utils';
 
 function App() {
   return (
