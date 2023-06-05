5cbd37c60d038164df25ed6ab12cd38e629b740f
meyota <meyota08@gmail.com>
2023-06-05 09:17:23 -0700

    props

---
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import "./App.css";
+import PropTypes from "prop-types";
+import CourseList from "../CourseList/CourseList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Login />
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
