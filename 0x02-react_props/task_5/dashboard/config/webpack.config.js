e26a13ab5c7a46e1d496013ef89cdaa343abc574
meyota <meyota08@gmail.com>
2023-06-05 13:48:35 -0700

    props

---
+++ 0x02-react_props/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+    },
+  },
+},
